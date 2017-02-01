a3b4408c6b5a7db0eca62ae0f4734ae26bafe878
James Kiesel <james.kiesel@gmail.com>
2017-02-02 10:31:15 +1300

    Fix failing spec

--- spec/rspec/services/group_service_spec.rb
+++ spec/rspec/services/group_service_spec.rb
@@ -6,17 +6,10 @@ describe 'GroupService' do
 
   describe 'create' do
 
-    it 'creates a new gift subscription for even experiences' do
-      group.experiences['bx_choose_plan'] = false
+    it 'creates a new gift subscription' do
       GroupService.create(group: group, actor: user)
       subscription = group.reload.subscription
       expect(subscription.kind.to_sym).to eq :gift
-    end
-
-    it 'creates no subscription for odd experiences' do
-      group.experiences['bx_choose_plan'] = true
-      GroupService.create(group: group, actor: user)
-      expect(group.reload.subscription).to be nil
     end
   end
 
